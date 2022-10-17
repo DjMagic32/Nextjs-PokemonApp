5e38f7a4a1317744290acfe58257e6cc5f90ecfe
DjMagic32 <garciawork3232@gmail.com>
2022-10-17 01:40:54 -0400

    add styles to cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import { Button } from '@nextui-org/react'
-import { NextPage } from 'next'
-import { Layout } from '../components/layouts'
-import { pokeApi } from '../api'
+import { NextPage } from 'next';
+import { Layout } from '../components/layouts';
+import { pokeApi } from '../api';
+import { Card, Col, Row, Button, Text, Grid } from "@nextui-org/react";
+
 
 
 // You should use getStaticProps when:
@@ -10,21 +11,88 @@ import { pokeApi } from '../api'
 //- The data can be publicly cached (not user-specific).
 //- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
 import { GetStaticProps } from 'next'
-import { PokemonListResponse } from '../interfaces/index'
+import { PokemonListResponse, SmallPokemon } from '../interfaces/index'
 
 const title: string = 'Listado de Pokemons'
 
-const Home: NextPage = ({ pokemons }) => {
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const Home: NextPage<Props> = ({ pokemons }) => {
   console.log(pokemons);
   return (
     <Layout title = {title}>
-      <ul>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-      </ul>
+      <Grid.Container gap={2} justify='flex-start'>
+
+        {
+          pokemons.map(({id, name, img}) => (
+            <Grid xs={5} sm={3} md={2} xl={1} key={id} >
+
+              <Card css={{ w: "100%", h: "350px" }}>
+                <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+                  <Col>
+                    <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+                      {id}
+                    </Text>
+                    <Text h3 color="white">
+                      {name}
+                    </Text>
+                  </Col>
+                </Card.Header>
+                <Card.Body css={{ p: 0 }}>
+                  <Card.Image
+                    src={img}
+                    width="130px"
+                    height="130px"
+                    
+                    alt={name}
+                  />
+                </Card.Body>
+                <Card.Footer
+                  isBlurred
+                  css={{
+                    position: "absolute",
+                    bgBlur: "#ffffff66",
+                    borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                    bottom: 0,
+                    zIndex: 1,
+                  }}
+                >
+                  <Row>
+                   {/*  <Col>
+                      <Text color="#000" size={12}>
+                        Available soon.
+                      </Text>
+                      <Text color="#000" size={12}>
+                        Get notified.
+                      </Text>
+                    </Col> */}
+                    <Col>
+                      <Row justify="center">
+                        <Button flat auto rounded color="secondary">
+                          <Text
+                            css={{ color: "inherit" }}
+                            size={12}
+                            weight="bold"
+                            transform="uppercase"
+                          >
+                            Add Favorites
+                          </Text>
+                        </Button>
+                      </Row>
+                    </Col>
+                  </Row>
+                </Card.Footer>
+              </Card>
+
+            </Grid>
+
+
+          ))
+        }
+     
+      </Grid.Container>
     </Layout>
   )
 }
@@ -37,10 +105,16 @@ const Home: NextPage = ({ pokemons }) => {
 export const getStaticProps: GetStaticProps = async (ctx) => {
 
   const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151') 
+  const pokemons: SmallPokemon[] = data.results.map((poke, i) => ({
+    ...poke,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i + 1}.svg`,
+
+  })) 
 
   return {
     props: {
-      pokemons: data.results
+      pokemons: pokemons,
       
     }
   }
